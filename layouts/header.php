8c5b718de34eee49694e8d5d29cb5c8072dd852a
Nausher Khan - i712700k <nausher.khan12@gmail.com>
2024-01-29 18:02:34 +0530

    meta  tage added

--- layouts/header.php
+++ layouts/header.php
@@ -1,12 +1,4 @@
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="googlebot" content="index">
-    <meta name="keywords" content="Bluenix bluenix Jamshedpur website development company bluenix.in bluenix.com Top 10 website company in jamshedpur top website development company in jamshedpur bluenix website development company india bluenix website development company jharkhand construction in indian organizqation L&T Construction bluenix website development">
-    <meta name="description" content="bluenix website development is here to make a difference. We stand out in several key ways. We prioritize a meticulous approach to every project, blending years of experience with a commitment to innovation. What truly distinguishes us is our unwavering dedication to safety protocols, ensuring not just quality results but also peace of mind for our clients. Additionally, our seamless project management and a track record of timely completions set us apart in the industry, demonstrating our reliability and consistency in delivering excellence.">
-    
-    <link rel="icon" href="img/short-icon.png" type="image/x-icon" />
     <?php 
         $title=isset($_GET['page']) ? $_GET['page'] : "Home";
         if($title=="home"){
@@ -22,8 +14,17 @@
         }else if($title=="contact"){
             $title="Contact";
         }
-        echo '<title>'.$title.' || Bluenix</title>';
     ?>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="googlebot" content="index">
+    <meta property="og:title" content="<?php echo $title.' || Bluenix'; ?>" />
+    <meta name="keywords" content="Bluenix bluenix Jamshedpur website development company bluenix.in bluenix.com Top 10 website company in jamshedpur top website development company in jamshedpur bluenix website development company india bluenix website development company jharkhand construction in indian organizqation L&T Construction bluenix website development bluenix.in ">
+    <meta name="description" content="bluenix website development is here to make a difference. We stand out in several key ways. We prioritize a meticulous approach to every project, blending years of experience with a commitment to innovation. What truly distinguishes us is our unwavering dedication to safety protocols, ensuring not just quality results but also peace of mind for our clients. Additionally, our seamless project management and a track record of timely completions set us apart in the industry, demonstrating our reliability and consistency in delivering excellence.">
+    
+    <link rel="icon" href="img/short-icon.png" type="image/x-icon" />
+    <?php echo '<title>'.$title.' || Bluenix</title>'; ?>
 
     <link href="css/font-awesome.min.css" rel="stylesheet">
 
